13a292dc18063f28a996ca781ed9aa3d121d8d1d
LunaRenilson <renilson.c.luna@gmail.com>
2025-01-11 09:08:39 -0300

    header feita. Sem animação

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import ErrorPage from './pages/ErrorPage.jsx'
+import Inicio from './pages/Inicio.jsx' 
+import Projetos from './pages/Projetos.jsx' 
+
+const routes = createBrowserRouter([
+  {
+    path: '/portfolio',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/portfolio',
+        element: <Inicio />,
+      },
+      {
+        path: '/portfolio/projetos',
+        element: <Projetos />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
