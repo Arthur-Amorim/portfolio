173a175270f1dd9dd8ef6a753da0bc7773349de6
LunaRenilson <renilson.c.luna@gmail.com>
2025-02-16 10:07:51 -0300

    categorização e containerização adicionada à modelagens

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,23 +1,29 @@
 import styles from './componentStyles/Images.module.css'
 import images from '../data/modelagens.json'
+
 function Images() {
      return (
           <div className={styles.imagesContainer}>
-               {
-                    images.map(({titulo, descricao, imagem}, index) => {
-                         return (
-                              <div key={index} className={styles.card}>
-                                   <img src={`/portfolio/modelagens/${imagem}`} alt={`Image ${index}`} key={index} />
-                                   <div className={styles.text}>
-                                        <h3>{titulo}</h3>
-                                        <p>{descricao}</p>
+               {images.map(({ categoria, imagens }, idCategoria) => (
+                    <div key={idCategoria}>
+                         <h2>{categoria}</h2>
+                         <hr />
+                         <div className={styles.categoryContainer}>
+                              {imagens.map(({titulo, descricao, imagem}, idImagem) => (
+                                   <div key={idImagem} className={styles.card}>
+                                        <img src={`/portfolio/modelagens/${imagem}`} alt={`Image ${idImagem}`} />
+                                        <div className={styles.text}>
+                                             <h3>{titulo}</h3>
+                                             <p>{descricao}</p>
+                                        </div>
                                    </div>
-                              </div>
-                         )
-                    })
-               }
+                              ))}
+                         </div>
+                    </div>
+               ))}
           </div>
      )
 }
 
 export default Images
+
